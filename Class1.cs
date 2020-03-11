2e5c03d428ec99a04bfd02d9bbf126efbb2b3493
Vadim Vorotilin <>
2020-03-11 19:46:05 +0200

    Third commit

--- Class1.cs
+++ Class1.cs
@@ -5,7 +5,7 @@ public class Class1 {
         for (int i = 0; i != 10; i++) {
             Console.WriteLine(i);
 
-            Console.WriteLine(i * 2);
+            Console.WriteLine(i * 3);
         }
     }
 }
