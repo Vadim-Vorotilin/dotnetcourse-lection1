f83a3cc056aeec2cd1473145dda72664a03a3ba7
Vadim Vorotilin <>
2020-03-11 19:37:33 +0200

    Second commit

--- Class1.cs
+++ Class1.cs
@@ -4,6 +4,8 @@ public class Class1 {
     public void Do() {
         for (int i = 0; i != 10; i++) {
             Console.WriteLine(i);
+
+            Console.WriteLine(i * 2);
         }
     }
 }
